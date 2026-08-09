c13074ac60c52ef9b2c001a9cad327365692b76f
lucasdetoffollemos@gmail.com <lucasdetoffollemos@gmail.com>
2026-08-09 15:29:39 +0100

    feat: creating doker file and docker compose files, add migration to autorun in program.cs

--- PaymentSimplifier/Program.cs
+++ PaymentSimplifier/Program.cs
@@ -32,12 +32,17 @@ namespace PaymentSimplifier
 
             var app = builder.Build();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.Migrate();
+            }
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
             }
-            app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
